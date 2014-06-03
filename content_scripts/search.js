c51dd89f30fd60f903a716be6200b9c0329d85d6
Sai Warang <cyprusad@gmail.com>
2014-06-03 23:53:27 +0530

    Add keyboard access to toggle search on/off

--- content_scripts/search.js
+++ content_scripts/search.js
@@ -16,3 +16,58 @@ function onSearchKeypress(e){
     }
   }
 }
+
+function showSearchBox(){
+  // show the search box
+  
+  if (!$searchBox){
+    // create the input field
+    $searchBox = $('<div style="background-color:#E4E8EC; border: 1px solid #999;  border-radius:10px; box-shadow: 5px 8px 10px #999; padding:15px; position:fixed;"><input type="text" style="font-size:3em;"/></div>');
+    $searchBox.appendTo('body');
+
+    $input = $searchBox.find('input');
+    $input
+      .blur(hideSearchBox)
+      .keypress(onSearchKeypress);
+  }
+
+  $searchBox
+    .show()
+    .position({
+      my: 'center',
+      at: 'center',
+      of: window
+    });
+
+  $input.focus();
+
+  searchBoxVisible = true;
+}
+
+function hideSearchBox(){
+  $searchBox.hide();
+  searchBoxVisible = false;
+}
+
+
+$(window).keypress(function(e){
+  if (!searchBoxVisible && e.which === 6){ // CTL-SHIFT-f
+    showSearchBox();
+  }
+});
+
+$(window).keydown(function(e){
+  if (searchBoxVisible && e.which === 27){ // the ESC key
+    hideSearchBox();
+  }
+});
+
+chrome.extension.onMessage.addListener(function(request){
+  if (request.action === 'toggleSearchBox'){
+    if (searchBoxVisible){
+      hideSearchBox();
+    } else {
+      showSearchBox();
+    }
+  }
+});
